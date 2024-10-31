0c48e59d6aee838290941a638aeca41050d80027
lhjack <lhjackgod@163.com>
2024-10-31 15:10:37 +0800

    add buffer layout descriptors for vertex buffers

--- MyHazel/src/Hazel/Application.cpp
+++ MyHazel/src/Hazel/Application.cpp
@@ -6,6 +6,46 @@
 namespace Hazel {
 	
 	Application* Application::s_Instance = nullptr;
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type) {
+		switch (type)
+		{
+		case ShaderDataType::Float:
+			return GL_FLOAT;
+			break;
+		case ShaderDataType::Float2:
+			return GL_FLOAT;
+			break;
+		case ShaderDataType::Float3:
+			return GL_FLOAT;
+			break;
+		case ShaderDataType::Float4:
+			return GL_FLOAT;
+			break;
+		case ShaderDataType::Mat3:
+			return GL_FLOAT;
+			break;
+		case ShaderDataType::Mat4:
+			return GL_FLOAT;
+			break;
+		case ShaderDataType::Int:
+			return GL_INT;
+			break;
+		case ShaderDataType::Int2:
+			return GL_INT;
+			break;
+		case ShaderDataType::Int3:
+			return GL_INT;
+			break;
+		case ShaderDataType::Int4:
+			return GL_INT;
+			break;
+		case ShaderDataType::Bool:
+			return GL_BOOL;
+			break;
+		}
+		HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
 	Application::Application()
 	{
 		HZ_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -14,26 +54,47 @@ namespace Hazel {
 		m_Window->SetEventCallback(HZ_BIND_EVENT_FN(Hazel::Application::OnEvent));
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverLayer(m_ImGuiLayer);
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 		unsigned int indices[3] = { 0, 1, 2 };
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+		{
+			BufferLayout layout = {
+				{ShaderDataType::Float3,"a_Position"},
+				{ShaderDataType::Float4, "a_Color"}
+			};
+			m_VertexBuffer->SetLayout(layout);
+		}
 		m_VertexBuffer->Bind();
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
-		m_IndexBuffer.reset(IndexBuffer::Create(indices, 3));
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout) {
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized? GL_TRUE:GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset
+				);
+			index++;
+		}
+		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
 		std::string vertexSrc = R"(
 			#version 330 core
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 			out vec3 v_Position;
+			out vec4 v_Color;
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position,1.0);
 			}
 		)";
@@ -41,9 +102,10 @@ namespace Hazel {
 			#version 330 core
 			layout(location = 0) out vec4 FragColor;
 			in vec3 v_Position;
+			in vec4 v_Color;
 			void main()
 			{
-				FragColor = vec4(v_Position * 0.5 + 0.5, 1.0);
+				FragColor = v_Color;
 			}
 		)";
 		m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
@@ -53,7 +115,6 @@ namespace Hazel {
 	{
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(HZ_BIND_EVENT_FN(Hazel::Application::OnWindowClose));
-		HZ_CORE_TRACE("{0}", e.ToString());
 		for (auto it = m_LayerStack.end()-1; it != m_LayerStack.begin(); it--) {
 			(*it)->OnEvent(e);
 			if (e.Handled)
--- MyHazel/src/Platform/OpenGL/OpenGLBuffer.h
+++ MyHazel/src/Platform/OpenGL/OpenGLBuffer.h
@@ -8,15 +8,18 @@ namespace Hazel {
 		virtual ~OpenGLVertexBuffer();
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
 	private:
+		BufferLayout m_Layout;
 		uint32_t m_RendererID;
 	};
 	class OpenGLIndexBuffer : public IndexBuffer {
 	public:
 		OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
 		virtual ~OpenGLIndexBuffer();
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
 		virtual uint32_t GetCount() const { return m_Count; }
 	private:
 		uint32_t m_RendererID;
