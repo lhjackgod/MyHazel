c8e17a952ba662be9995f4253be370e694cb51db
lhjack <lhjackgod@163.com>
2024-11-12 20:49:32 +0800

    Changed ScriptableEntity functions to be virtual

--- Hazelnut/src/EditorLayer.cpp
+++ Hazelnut/src/EditorLayer.cpp
@@ -31,7 +31,8 @@ namespace Hazel {
 		public:
 			void OnCreate()
 			{
-
+				auto& transform = GetComponent<TransformComponent>().Transform;
+				transform[3][0] = rand() % 10 - 5.0f;
 			}
 			void OnDestroy()
 			{
@@ -53,6 +54,7 @@ namespace Hazel {
 			}
 		};
 		m_CameraEntity.AddComponent<NativeScriptComponent>().Bind<CameraController>();
+		m_SecondCamra.AddComponent<NativeScriptComponent>().Bind<CameraController>();
 	}
 	void EditorLayer::OnDetach()
 	{
